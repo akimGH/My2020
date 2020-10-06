ef8879c0e7231e966606d8753510ed978b18db53
akimGH <barushevskiy.akim@mail.ru>
2020-10-06 17:33:57 +1000

    Final

--- main.py
+++ main.py
@@ -4,5 +4,23 @@ print(2 + 4 / 6 * 10)
 print(9 // 4)
 name = 10
 print(name)
-i = 'Это мои первые строчки кода'
+i = "Это мои первые строчки кода"
 print(i)
+n = "10"
+m = 20
+print(int(n) + m)
+print(1)
+print("1")
+print(n + str(m))
+print(f"{n}{m}")
+j = f"{m}-''это переменная'"
+print(j)
+print(n, m)
+print(n, f"\n{m}")
+name1 = input("Введите число ")
+name2 = input("Введите число ")
+print(name1 + name2)
+name1 = int(input("Введите число "))
+name2 = int(input("Введите число "))
+print(name1 + name2)
+m += 1
