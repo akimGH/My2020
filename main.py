e7b31e8ccb2577a41e88b7257aa8ba62ab85aade
Akim <72432836+akimGH@users.noreply.github.com>
2020-10-13 17:30:55 +1000

    Update main.py

--- main.py
+++ main.py
@@ -69,3 +69,49 @@ o = 0
 while o < 10:
     print(o)
     o += 1
+
+# Next
+# + Images
+
+import pygame
+import random
+import os
+
+num = random.randint(0, 100)
+print(num)
+
+W = 480
+H = 360
+SILVER = (192, 192, 192)
+BLACK = (0, 0, 0)
+
+pygame.init()
+pygame.display.set_caption("Угадай число")
+pygame.mouse.set_visible(False)
+screen = pygame.display.set_mode((W, H))
+
+path = os.path.dirname(os.path.abspath(__file__))
+bg = pygame.image.load(os.path.join(path, "Image/room.png"))
+bg_rect = bg.get_rect(topleft=(0, 0))
+cat = pygame.image.load(os.path.join(path, "Image/cat.png"))
+cat_rect = cat.get_rect(topleft=(70, 220))
+dog = pygame.image.load(os.path.join(path, "Image/dog.png"))
+dog_rect = dog.get_rect(topleft=(410, 220))
+owl = pygame.image.load(os.path.join(path, "Image/owl.png"))
+owl_rect = owl.get_rect(topleft=(210, 120))
+dialog = pygame.image.load(os.path.join(path, "Image/dialog.png"))
+dialog_rect = owl.get_rect()
+dialog_cat_pos = (cat_rect.x, cat_rect.y - dialog_rect.h)
+dialog_dog_pos = (dog_rect.x - dialog_rect.w // 2, dog_rect.y - dialog_rect.h)
+
+run = True
+while run:
+    for e in pygame.event.get():
+        if e.type == pygame.QUIT or e.type == pygame.KEYDOWN and e.key == pygame.K_ESCAPE:
+            run = False
+
+    screen.blit(bg, bg_rect)
+    screen.blit(cat, cat_rect)
+    screen.blit(dog, dog_rect)
+    screen.blit(owl, owl_rect)
+    pygame.display.update()
